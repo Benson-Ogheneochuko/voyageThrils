f0a80dbae7f07d49dc9b6d853dee2219b2cec6ee
Nockk <ob.ogheneochuko@gmail.com>
2024-08-17 19:04:45 +0100

    fix: build config

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,6 +1,6 @@
-import { config } from "../config/env";
-import { app } from "./index";
+import { config } from '../config/env';
+import { app } from './index';
 const port = config.port;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
