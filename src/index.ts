1555d6b0d101a1c5758aa81e26c1197b9761075f
Nockk <ob.ogheneochuko@gmail.com>
2024-08-18 16:26:50 +0100

    fix: cors origin for deployment

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ import cors from 'cors';
 export const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: /http:\/\/localhost:\d+/  // Regex to match any localhost origin
+  origin: [
+      /http:\/\/localhost:\d+/,  // Regex to match any localhost origin
+      'https://main--echangerateproject.netlify.app'
+  ]
 }));
 
 app.get('/', (req, res) => {
